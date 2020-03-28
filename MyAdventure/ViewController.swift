7bb2e9b91a2bee2287ae41d014dbf93c15c01581
Turki <alsougit@gmail.com>
2020-03-28 12:11:01 +0300

    Initial Commit

---
+++ MyAdventure/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyAdventure
+//
+//  Created by Turki Sougi on 28/03/2020.
+//  Copyright © 2020 Turki Sougi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
